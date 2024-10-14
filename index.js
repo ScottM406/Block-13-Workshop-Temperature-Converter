3e0c3e3f234acf8a960cb0dcb12e70a574ec62d9
Scott <scottyjmt13@gmail.com>
2024-10-14 09:12:26 -0600

    final commit - changed functions so that it can take any input, rather than just the user's input

--- index.js
+++ index.js
@@ -3,34 +3,30 @@
 const userTemperature = prompt('Please enter your local temperature in Fahrenheit. We will convert the temperature to Celsius.')
 const userTempAsNum = Number(userTemperature);
 
-const convertToCelsius = () => {
-  return (userTempAsNum -32)*(5/9);
+const convertToCelsius = (tempToConvert) => {
+  return (tempToConvert -32)*(5/9);
  
 }
 
-convertToCelsius();
-
-const celsiusTemp = convertToCelsius();
+const celsiusTemp = convertToCelsius(userTempAsNum);
 
 // Below: A function generates a description for the temperature given by the user.
 
-const describeTmperature = (range1,range2,range3,range4,range5) => {
-  if (Number(userTemperature) < 32)
-    return(`You entered ${userTempAsNum} degrees Farenheit, that is ${celsiusTemp} degrees Celcius. That is very cold!`);
-  else if (Number(userTemperature) < 64 && Number(userTemperature) >= 32)
-    return(`You entered ${userTempAsNum} degrees Farenheit, that is ${celsiusTemp} degrees Celcius. That is cold!`);
-  else if (Number(userTemperature) < 86 && Number(userTemperature) >= 64)
-    return(`You entered ${userTempAsNum} degrees Farenheit, that is ${celsiusTemp} degrees Celcius. That is warm!`);
-  else if (Number(userTemperature) <100 && Number(userTemperature) >= 86)
-    return(`You entered ${userTempAsNum} degrees Farenheit, that is ${celsiusTemp} degrees Celcius. That is hot!`);
-  else if (Number(userTemperature) >=100)
-    return(`You entered ${userTempAsNum} degrees Farenheit, that is ${celsiusTemp} degrees Celcius. That is very hot, wear sunscreen!`);
+const describeTmperature = (fahrenheit) => {
+  if (Number(fahrenheit) < 32)
+    return(`You entered ${fahrenheit} degrees Fahrenheit, that is ${celsiusTemp} degrees Celcius. That is very cold!`);
+  else if (Number(fahrenheit) < 64 && Number(userTemperature) >= 32)
+    return(`You entered ${fahrenheit} degrees Fahrenheit, that is ${celsiusTemp} degrees Celcius. That is cold!`);
+  else if (Number(fahrenheit) < 86 && Number(userTemperature) >= 64)
+    return(`You entered ${fahrenheit} degrees Fahrenheit, that is ${celsiusTemp} degrees Celcius. That is warm!`);
+  else if (Number(fahrenheit) <100 && Number(userTemperature) >= 86)
+    return(`You entered ${fahrenheit} degrees Fahrenheit, that is ${celsiusTemp} degrees Celcius. That is hot!`);
+  else if (Number(f) >=100)
+    return(`You entered ${fahrenheit} degrees Fahrenheit, that is ${celsiusTemp} degrees Celcius. That is very hot, wear sunscreen!`);
 
 }
 
-describeTmperature();
-
-const message = describeTmperature();
+const message = describeTmperature(userTemperature);
 
 // Below: a function sends an alert based off of the number the user entered. It contains the description generated above.
 
